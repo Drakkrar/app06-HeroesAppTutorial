1b5222310633684bbe83c06586c2f7a457341b01
Francisco Ramos S <jramossepulveda3@gmal.com>
2022-04-30 19:21:20 -0500

    Fin de tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,7 +12,6 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService, 
     private messageService: MessageService) { }
@@ -22,13 +21,24 @@ export class HeroesComponent implements OnInit {
   }
 
   // Methods
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+
+    if (!name) { return; }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 
 }
